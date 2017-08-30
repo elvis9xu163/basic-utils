7732e0a1291e9fe3490a1b6ce6c4451997bb2b36
elvis.xu <elvis9xu@163.com>
2017-08-30 09:25:34 +0800

    Add AssertUtils for assertions and deprecate relative methods in ObjectUtils

---
+++ src/main/java/com/xjd/utils/basic/AssertUtils.java
@@ -0,0 +1,58 @@
+package com.xjd.utils.basic;
+
+import java.util.Objects;
+
+/**
+ * @author elvis.xu
+ * @since 2017-08-29 19:19
+ */
+public abstract class AssertUtils {
+
+	public static <T> void assertNonNull(T obj, String msg) {
+		Objects.requireNonNull(obj, msg);
+	}
+
+	public static <T> void assertNonNull(T obj) {
+		Objects.requireNonNull(obj);
+	}
+
+	public static <T> void assertArgumentNonNull(T obj, String msg) {
+		if (obj == null) {
+			throw new IllegalArgumentException(msg);
+		}
+	}
+
+	public static <T> void assertArgumentNonNull(T obj) {
+		if (obj == null) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void assertArgumentGreaterEqualThan(Integer number, int refer, String msg) {
+		assertArgumentNonNull(number, msg);
+		if (number < refer) {
+			throw new IllegalArgumentException(msg);
+		}
+	}
+
+	public static void assertArgumentGreaterEqualThan(Long number, long refer, String msg) {
+		assertArgumentNonNull(number, msg);
+		if (number < refer) {
+			throw new IllegalArgumentException(msg);
+		}
+	}
+
+	public static void assertArgumentLessEqualThan(Integer number, int refer, String msg) {
+		assertArgumentNonNull(number, msg);
+		if (number > refer) {
+			throw new IllegalArgumentException(msg);
+		}
+	}
+
+	public static void assertArgumentLessEqualThan(Long number, long refer, String msg) {
+		assertArgumentNonNull(number, msg);
+		if (number > refer) {
+			throw new IllegalArgumentException(msg);
+		}
+	}
+}
--- src/main/java/com/xjd/utils/basic/ObjectUtils.java
+++ src/main/java/com/xjd/utils/basic/ObjectUtils.java
@@ -8,20 +8,21 @@ import java.util.Objects;
  */
 public abstract class ObjectUtils {
 
+	@Deprecated
 	public static <T> T requireNonNull(T obj, String msg) {
 		return Objects.requireNonNull(obj, msg);
 	}
-
+	@Deprecated
 	public static <T> T requireNonNull(T obj) {
 		return Objects.requireNonNull(obj);
 	}
-
+	@Deprecated
 	public static <T> T  requireArgumentNonNull(T obj, String msg) {
 		if (obj == null)
 			throw new IllegalArgumentException(msg);
 		return obj;
 	}
-
+	@Deprecated
 	public static <T> T  requireArgumentNonNull(T obj) {
 		if (obj == null)
 			throw new IllegalArgumentException();
