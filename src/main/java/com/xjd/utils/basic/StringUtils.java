335d47990e47d33e929e8490d81c235c357a9d51
elvis.xu <elvis9xu@163.com>
2017-09-12 10:50:31 +0800

    StringUtils add methods for String[] processing: split(), trimElement(), toLowerCase(), trimNull() and so on.

--- src/main/java/com/xjd/utils/basic/StringUtils.java
+++ src/main/java/com/xjd/utils/basic/StringUtils.java
@@ -1,5 +1,9 @@
 package com.xjd.utils.basic;
 
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 /**
  * @author elvis.xu
  * @since 2017-08-16 16:07
@@ -42,4 +46,56 @@ public abstract class StringUtils {
 	public static String toUpperCase(String str) {
 		return str == null ? str : str.toUpperCase();
 	}
+
+	public static String[] split(String txt, String regex) {
+		if (txt == null) return new String[0];
+		return txt.split(regex, -1); // 默认使用-1
+	}
+
+	public static String[] trimElement(String[] strArray) {
+		return map(strArray, StringUtils::trim);
+	}
+
+	public static String[] trimElementToNull(String[] strArray) {
+		return map(strArray, StringUtils::trimToNull);
+	}
+
+	public static String[] trimElementToEmpty(String[] strArray) {
+		return map(strArray, StringUtils::trimToEmpty);
+	}
+
+	public static String[] toLowerCase(String[] strArray) {
+		return map(strArray, StringUtils::toLowerCase);
+	}
+
+	public static String[] toUpperCase(String[] strArray) {
+		return map(strArray, StringUtils::toUpperCase);
+	}
+
+	public static String[] trimNull(String[] strArray) {
+		return filter(strArray, s -> s != null);
+	}
+
+	public static String[] trimEmpty(String[] strArray) {
+		return filter(strArray, s -> !isEmpty(s));
+	}
+
+	public static String[] trimBlank(String[] strArray) {
+		return filter(strArray, s -> !isBlank(s));
+	}
+
+	public static String[] map(String[] strArray, Function<String, String> mapper) {
+		if (strArray != null && strArray.length > 0) {
+			return Arrays.stream(strArray).map(mapper).toArray(String[]::new);
+		}
+		return new String[0];
+	}
+
+	public static String[] filter(String[] strArray, Predicate<String> predicate) {
+		if (strArray != null && strArray.length > 0) {
+			return Arrays.stream(strArray).filter(predicate).toArray(String[]::new);
+		}
+		return new String[0];
+	}
+
 }
