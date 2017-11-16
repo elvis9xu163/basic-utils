8593668364eb33c61bccb997a9c2c55a998c8806
elvis.xu <elvis9xu@163.com>
2017-11-16 10:32:39 +0800

    CollectionUtils: 	asMap:  support genericity 	addMultiValue: add an value to the key mapped collection

--- src/main/java/com/xjd/utils/basic/CollectionUtils.java
+++ src/main/java/com/xjd/utils/basic/CollectionUtils.java
@@ -1,5 +1,7 @@
 package com.xjd.utils.basic;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,13 +10,25 @@ import java.util.Map;
  * @since 2017-11-16 10:05
  */
 public abstract class CollectionUtils {
-	public static Map asMap(Object... keyOrVal) {
+	public static <K, V> Map<K, V> asMap(Object... keyOrVal) {
 		if (keyOrVal == null) return null;
 		if (keyOrVal.length % 2 != 0) throw new IllegalArgumentException("the length of params must be even.");
-		Map map = new HashMap(keyOrVal.length / 2);
+		Map<K, V> map = new HashMap<K, V>(keyOrVal.length / 2);
 		for (int i = 0; i < keyOrVal.length; i += 2) {
-			map.put(keyOrVal[i], keyOrVal[i + 1]);
+			map.put((K) keyOrVal[i], (V) keyOrVal[i + 1]);
 		}
 		return map;
+	}
+
+	public static <K, V> void addMultiValue(Map<K, Collection<V>> map, K key, V value) {
+		Collection<V> vs = map.get(key);
+		if (vs == null) {
+			vs = new ArrayList<V>();
+			Collection<V> existVs = map.putIfAbsent(key, vs);
+			if (existVs != null) {
+				vs = existVs;
+			}
+		}
+		vs.add(value);
 	}
 }
---
+++ src/test/java/com/xjd/utils/basic/CollectionUtilsExample.java
@@ -0,0 +1,15 @@
+package com.xjd.utils.basic;
+
+import java.util.Map;
+
+/**
+ * @author elvis.xu
+ * @since 2017-11-16 10:16
+ */
+public class CollectionUtilsExample {
+	public static void main(String[] args) {
+//		Map<String, String> map = CollectionUtils.<String, String>asMap("key1", "val1", "key2", "val2", "key3", 20L);
+		Map<String, String> map = CollectionUtils.asMap("key1", "val1", "key2", "val2", "key3", 20L);
+		System.out.println(JsonUtils.toJson(map));
+	}
+}
