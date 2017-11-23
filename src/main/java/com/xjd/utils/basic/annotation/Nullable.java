e055ce8863f85a33792ece5536410395a4c06503
elvis.xu <elvis9xu@163.com>
2017-11-23 11:21:52 +0800

    add @Nullable @NonNull for parameters indicater

---
+++ src/main/java/com/xjd/utils/basic/annotation/Nullable.java
@@ -0,0 +1,13 @@
+package com.xjd.utils.basic.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author elvis.xu
+ * @since 2017-11-23 10:50
+ */
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Nullable {
+}
